764c6b7a6377df2abefa1de13f73af810d70f38a
ShawnPhang <palxiao@vip.qq.com>
2021-09-11 16:30:34 +0800

    feat: init

---
+++ packages/test/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: ShawnPhang
+ * @Date: 2021-09-11 16:29:45
+ * @Description: 
+ * @LastEditors: ShawnPhang
+ * @LastEditTime: 2021-09-11 16:30:00
+ * @site: book.palxp.com / blog.palxp.com
+ */
+// this is a test
