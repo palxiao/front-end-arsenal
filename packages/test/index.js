bc78a9290cabf78e7b1cd37c0ed73ea61ac03d33
ShawnPhang <palxiao@vip.qq.com>
2021-09-11 16:39:30 +0800

    test: version change

--- packages/test/index.js
+++ packages/test/index.js
@@ -3,7 +3,8 @@
  * @Date: 2021-09-11 16:29:45
  * @Description: 
  * @LastEditors: ShawnPhang
- * @LastEditTime: 2021-09-11 16:30:00
+ * @LastEditTime: 2021-09-11 16:38:47
  * @site: book.palxp.com / blog.palxp.com
  */
 // this is a test
+// change Test
