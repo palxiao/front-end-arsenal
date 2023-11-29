17ea463869641b6a9acd47ef91811114f14b6383
ShawnPhang <palxiao@vip.qq.com>
2023-11-29 19:30:43 +0800

    docs(custom): update angle handle

--- docs/assets/example1.c6a258ca.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./index.0a7029a8.js";import{_ as t,d as n,r,o as c,c as p}from"./index.9048c798.js";const s=n({components:{comp:e},setup(){return{}}});function a(m,_,f,i,d,u){const o=r("comp");return c(),p(o)}var $=t(s,[["render",a]]);export{$ as default};
---
+++ docs/assets/javascript.5cd8e8ba.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript.aab64c82.js";import"./example1.44aef8c5.js";import"./index.78fd87f9.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.38.0(0e330ae453813de4e6cf272460fb79c7117073d0)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var r=t,a={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{r as conf,a as language};
